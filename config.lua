550537f790c8b0e691d23965fb738776c2bf6277
TeleMrjX <mr.jalali76@gmail.com>
2017-02-06 17:54:09 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = "235796629:AAEKsTvofbf3IyYfXfrp4PCcJtHuXEfF5uk"
+bot_token = "295868097:AAFXfx7zUVFr3TpLbPgq8-b-jM6pqxTwajw"
 send_api = "https://api.telegram.org/bot"..bot_token
-bot_version = "2.5"
-sudo_id = 180910633
+bot_version = "2"
+sudo_id = 193156389
